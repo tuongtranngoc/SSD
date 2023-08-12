90eb33ec4a78b73ee2c3a9af156999d133d30d9b
tuongtranngoc <ngoctuongwindy@gmail.com>
2023-08-12 07:25:12 +0700

    update: default boxes

--- ssd/data/default_boxes.py
+++ ssd/data/default_boxes.py
@@ -11,17 +11,17 @@ from collections import defaultdict
 from ..utils import cfg
 
 class DefaultBoxes:
+    m = cfg.default_boxes.fm_sizes
+    s_min = cfg.default_boxes.s_min
+    s_max = cfg.default_boxes.s_max
+    ratios = cfg.default_boxes.respect_ratio
+    im_size = cfg.models.image_size
 
     @classmethod
-    def build_default_boxes(self):
-        m = cfg.default_boxes.fm_sizes
-        s_min = cfg.default_boxes.s_min
-        s_max = cfg.default_boxes.s_max
-        ratios = cfg.default_boxes.respect_ratio
-
+    def build_default_boxes(cls):
         df_bboxes = defaultdict()
 
-        for k in m:
+        for k in cls.m:
             df_bboxes[k] = torch.zeros(size=(k, k, 6 , 4))
 
             idxs_i = torch.arange(k)
@@ -36,20 +36,25 @@ class DefaultBoxes:
             df_bboxes[k][..., :2] = xcyc
 
             wh_ratios = []
-            s_k = s_min + (s_max - s_min) * (k - 1) / (len(m) - 1)
-            for i, a_r in enumerate(ratios):
+            s_k = cls.s_min + (cls.s_max - cls.s_min) * (k - 1) / (len(cls.m) - 1)
+            for i, a_r in enumerate(cls.ratios):
                 if a_r == 1:
-                    s_k_1 = s_min + (s_max - s_min) * (k + 1 - 1) / (len(m) - 1)
-                    s_k_0 = (s_k * s_k_1)
-                    w_k = s_k_0 * math.sqrt(a_r)
-                    h_k = s_k_0 / math.sqrt(a_r)
+                    s_k_1 = cls.s_min + (cls.s_max - cls.s_min) * (k + 1 - 1) / (len(cls.m) - 1)
+                    s_k_0 = math.sqrt(s_k * s_k_1)
+                    w_k = s_k_0 * math.sqrt(a_r) / cls.im_size
+                    h_k = s_k_0 / math.sqrt(a_r) / cls.im_size
                     wh_ratios.append([w_k, h_k])
-                w_k = s_k * math.sqrt(a_r)
-                h_k = s_k / math.sqrt(a_r)
-                wh_ratios.append([w_k, h_k])
 
+                w_k = s_k * math.sqrt(a_r) / cls.im_size
+                h_k = s_k / math.sqrt(a_r) / cls.im_size
+                wh_ratios.append([w_k, h_k])
             df_bboxes[k][..., 2:] = torch.tensor(wh_ratios)
+
         return df_bboxes
+    
+    @classmethod
+    def decode_defaultbox(cls, bboxes):
+        pass
     
 
 if __name__ == "__main__":
